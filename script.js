1c176af60a2025df5a67051263d9388a6ece5be7
ameerkhansaeed <149019612+ameerkhansaeed@users.noreply.github.com>
2023-10-28 00:20:57 +0500

    Add files via upload
    A major improvement for dark and ligth theme toggle.

--- script.js
+++ script.js
@@ -1,12 +1,62 @@
-function themeChange() {
-  var element = document.body;
-  element.classList.toggle("dark-theme");
+function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+  if (localStorageTheme !== null) {
+    return localStorageTheme;
+  }
 
-  var icon = document.getElementById("icon");
-  if(document.body.classList.contains("dark-theme")) {
-    icon.src = "icons/light-mode.png"
+  if (systemSettingDark.matches) {
+    return "dark";
   }
-  else{
-    icon.src = "icons/dark-mode.png"
-  }
+
+  return "light";
 }
+
+function updateButton({ buttonEl, isDark }) {
+  const newCta = isDark ? "<img src=\./icons/light-mode.png\>" : "<img src=\./icons/dark-mode.png\>";
+  // use an aria-label if you are omitting text on the button
+  // and using a sun/moon icon, for example
+  buttonEl.setAttribute("aria-label", newCta);
+  buttonEl.innerHTML = newCta;
+}
+
+/**
+* Utility function to update the theme setting on the html tag
+*/
+function updateThemeOnHtmlEl({ theme }) {
+  document.querySelector("html").setAttribute("data-theme", theme);
+}
+
+
+/**
+* On page load:
+*/
+
+/**
+* 1. Grab what we need from the DOM and system settings on page load
+*/
+const button = document.querySelector("[data-theme-toggle]");
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+/**
+* 2. Work out the current site settings
+*/
+let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
+
+/**
+* 3. Update the theme setting and button text accoridng to current settings
+*/
+updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
+updateThemeOnHtmlEl({ theme: currentThemeSetting });
+
+/**
+* 4. Add an event listener to toggle the theme
+*/
+button.addEventListener("click", () => {
+  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+  localStorage.setItem("theme", newTheme);
+  updateButton({ buttonEl: button, isDark: newTheme === "dark" });
+  updateThemeOnHtmlEl({ theme: newTheme });
+
+  currentThemeSetting = newTheme;
+});
